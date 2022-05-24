98db8f3b206ed2ec17e1c5357ed182a0c03310d6
Kevin Palis <kdp44@cornell.edu>
2022-05-23 23:57:26 -0400

    Unit tests added.

--- compass/test/bc_unit_test.py
+++ compass/test/bc_unit_test.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 '''
-	Unit tests for TriforceLink
+	Unit tests for Boston Compass
 
-    Run using this command at the main triforce directory: python3 -m pytest -v
+    Run using this command at the main compass directory: python3 -m pytest -v
     Note that these are very simple tests just to show how I would go about developing such a module.
-    If given more time, and if this is a module that will eventually process prod data, I would add more tests
-    that will also check the number of entso-gppd-platts mapping based off of a known good result. Lastly, I would add more fixture data.
+    If given more time, and if this is a module that will eventually process more data, I would add more tests that will cover a good variety of real-life use-cases.
     But for the purpose of this coding exercise, I think the following tests are sufficient.
 
     @author Kevin Palis <kevin.palis@gmail.com>
@@ -14,26 +13,26 @@ from compass import compass
 from compass.util.bc_utility import ReturnCodes
 import pytest
 
-#Test running the mapper with no parameters, ie. everything set as default
-def test_map_data_main_all_defaults():
-    assert mapping.main([]) == ReturnCodes.SUCCESS
+#Test running the compass with no parameters, ie. everything set as default - AKA Happy-path
+def test_compass_main_all_defaults():
+    assert compass.main([]) == ReturnCodes.SUCCESS
 
-#Test running the mapper with an invalid ENTSO file
-def test_map_data_invalid_entso_file():
-    assert mapping.main(['-e', 'invalid_entso_test.csv']) == ReturnCodes.ERROR_PARSING_FILE
+#Test running the compass with no parameters but verbose
+def test_compass_main_all_defaults_verbose():
+    assert compass.main(['-v']) == ReturnCodes.SUCCESS
 
-#Test running the mapper with an invalid GPPD file
-def test_map_data_invalid_gppd_file():
-    assert mapping.main(['-g', 'invalid_gppd_test.csv']) == ReturnCodes.ERROR_PARSING_FILE
+#Test running the compass with all stops printed
+def test_compass_main_show_all_stops():
+    assert compass.main(['-a']) == ReturnCodes.SUCCESS
 
-#Test running the mapper with an invalid Platts file
-def test_map_data_invalid_platts_file():
-    assert mapping.main(['-p', 'invalid_platts_test.csv']) == ReturnCodes.ERROR_PARSING_FILE
+#Test running the compass with an invalid source stop
+def test_compass_invalid_source_stop():
+    assert compass.main(['-s', 'NopeNope']) == ReturnCodes.INVALID_VERTEX
 
-#Test running the mapper with higher verbosity
-def test_map_data_main_all_defaults_verbose():
-    assert mapping.main(['-v']) == ReturnCodes.SUCCESS
+#Test running the compass with an invalid destination stop
+def test_compass_invalid_destination_stop():
+    assert compass.main(['-d', 'DoesNotExist']) == ReturnCodes.INVALID_VERTEX
 
-#Test running the mapper with plant_names normalization turned on
-def test_map_data_with_normalize_names():
-    assert mapping.main(['-n', 'True']) == ReturnCodes.SUCCESS
+#Test running the compass with invalid API key
+def test_compass_invalid_api_key():
+    assert compass.main(['-k', 'InvalidKey']) == ReturnCodes.API_CALL_FAILED
